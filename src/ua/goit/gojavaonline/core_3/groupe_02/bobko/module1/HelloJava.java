ab01c6aeddc81f11cf64cfbea9150ce162850ce8
RomanBobko <3048222@gmail.com>
2016-05-30 11:20:02 +0300

    commit module01

---
+++ src/ua/goit/gojavaonline/core_3/groupe_02/bobko/module1/HelloJava.java
@@ -0,0 +1,7 @@
+package ua.goit.gojavaonline.core_3.groupe_02.bobko.module1;
+
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("Hi, I'm your first Java program! Stay tuned!");
+    }
+}
