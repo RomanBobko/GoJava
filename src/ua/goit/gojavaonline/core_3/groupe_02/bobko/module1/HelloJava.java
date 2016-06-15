f4f9285ce5b4b750db845a44fd0b8734dbcd6b9e
Dmitrij0 <dmitrijlenchuk@gmail.com>
2016-06-15 09:59:52 +0300

    First note

--- src/ua/goit/gojavaonline/core_3/groupe_02/bobko/module1/HelloJava.java
+++ src/ua/goit/gojavaonline/core_3/groupe_02/bobko/module1/HelloJava.java
@@ -1,6 +1,12 @@
 package ua.goit.gojavaonline.core_3.groupe_02.bobko.module1;
 
 public class HelloJava {
+    /*ToDo
+    * Задание было вывести строку
+      * "Hi, I'm your first Java program! Stay tuned!"
+      * а не
+      * Hi, I'm your first Java program! Stay tuned!
+    * */
     public static void main(String[] args) {
         System.out.println("Hi, I'm your first Java program! Stay tuned!");
     }
