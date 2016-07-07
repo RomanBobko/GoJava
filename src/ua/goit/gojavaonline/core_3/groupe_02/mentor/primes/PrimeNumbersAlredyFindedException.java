5881967b429d4cf4733b6f099eb39580630270a0
RomanBobko <3048222@gmail.com>
2016-07-08 00:00:03 +0300

    delete

--- src/ua/goit/gojavaonline/core_3/groupe_02/mentor/primes/PrimeNumbersAlredyFindedException.java
+++
@@ -1,7 +0,0 @@
-package ua.goit.gojavaonline.core_3.groupe_02.mentor.primes;
-
-/**
- * Created by dima on 05.07.2016.
- */
-public class PrimeNumbersAlredyFindedException extends Exception {
-}
--- src/ua/goit/gojavaonline/core_3/groupe_02/mentor/readme.txt
+++
@@ -1,7 +0,0 @@
-Финальный проект курса
-
-ua.goit.online.java.core_03
-
-автор:  ментор Дмитрий
-        dima.kiev@gmail.com
-
